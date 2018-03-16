7b9276723b2a46225ce712c10d84cb290b53241a
Grzegorz Sagadyn <grzegorz.sagadyn@speednet.pl>
2018-03-16 09:12:26 +0100

    Add project

---
+++ BalticMuseums/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BalticMuseums
+//
+//  Created by Grzegorz Sagadyn on 16.03.2018.
+//  Copyright © 2018 Grzegorz Sagadyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '11.2'
+inhibit_all_warnings!
+use_frameworks!
+
+target 'BalticMuseums' do
+  # Pods for BalticMuseums
+
+  target 'BalticMuseumsTests' do
+    inherit! :search_paths
+  end
+
+  target 'BalticMuseumsUITests' do
+    inherit! :search_paths
+  end
+end
