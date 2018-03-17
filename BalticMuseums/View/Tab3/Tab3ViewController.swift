95c2929b971b079dcbc735f867ae12156c68d792
Grzegorz Sagadyn <grzegorz.sagadyn@speednet.pl>
2018-03-17 16:09:26 +0100

    Add SceneView

--- BalticMuseums/View/Tab3/Tab3ViewController.swift
+++ BalticMuseums/View/Tab3/Tab3ViewController.swift
@@ -8,11 +8,57 @@
 
 import UIKit
 import Snapify
+import SceneKit
+import ARKit
+import Vision
 
-class Tab3ViewController: UserInterfaceLayer.Controller<Tab3Presenter>, UIStoryboardInstantiate  {
+class Tab3ViewController: UIViewController, UIStoryboardInstantiate {
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var detectedDataAnchor: ARAnchor?
+    var processing = false
+    
+    // MARK: - View Controller Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .darkGray
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.showsStatistics = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
     }
 }
+
+// -----------------------------------------------------------------------------
+// MARK: - ARSessionDelegate
+// -----------------------------------------------------------------------------
+
+extension Tab3ViewController: ARSessionDelegate {
+    internal func session(_ session: ARSession, didUpdate frame: ARFrame) {
+    }
+}
+
+// -----------------------------------------------------------------------------
+// MARK: - ARSCNViewDelegate
+// -----------------------------------------------------------------------------
+
+extension Tab3ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        return nil
+    }
+}
+
+
