75071bb9439c2d368b88f0b0713163824d8bcd4f
Maciej Duraziński <durazinskimaciej@gmail.com>
2018-03-17 14:27:40 +0100

    adds assets

---
+++ BalticMuseums/View/Tab1/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  BalticMuseums
+//
+//  Created by Maciej Durazinski on 17.03.2018.
+//  Copyright © 2018 Grzegorz Sagadyn. All rights reserved.
+//
+
+import UIKit
+
+class MapView: UIView {
+	
+	@IBOutlet weak var mapImageView: UIImageView!
+	
+	override var intrinsicContentSize: CGSize {
+		return mapImageView.intrinsicContentSize
+	}
+
+}
