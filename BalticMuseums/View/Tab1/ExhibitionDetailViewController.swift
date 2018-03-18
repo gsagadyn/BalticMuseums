28b77850cb26d927c71006fcc2a8788d4aa34d33
Maciej Duraziński <durazinskimaciej@gmail.com>
2018-03-18 09:24:13 +0100

    adds player

--- BalticMuseums/View/Tab1/ExhibitionDetailViewController.swift
+++ BalticMuseums/View/Tab1/ExhibitionDetailViewController.swift
@@ -8,46 +8,18 @@
 
 import UIKit
 
-class ExhibitionDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ExhibitionDetailViewController: UIViewController {
 	
-	@IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var closeButton: UIButton!
 	var selectedExibition: Exhibition?
-	var header: ExhibitionDetailHeaderView? {
-		didSet {
-			updateView()
-		}
-	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		tableView.layoutIfNeeded()
-		header = ExhibitionDetailHeaderView(tableView: tableView, minHeight: 100, maxHeight: 300)
-		
-		closeButton.setImage(UIImage(named: "close")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate), for: UIControlState.normal)
-		closeButton.tintColor = UIColor(named: "main")
-		
-		tableView.backgroundColor = .clear
 	}
 	
 	@IBAction func closeAction(_ sender: Any) {
 		dismiss(animated: true, completion: nil)
-	}
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 2
-	}
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
-		return cell
-	}
-	
-	func updateView() {
-		header?.imageView.image = UIImage.exhibitionImageForType(selectedExibition!)
-		header?.label.text = String.exhibitionTitleForType(selectedExibition!)
-
 	}
 	
 }
